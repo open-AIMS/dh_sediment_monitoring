dc465cefc8b0aa6198c99cceb76ecd4ea14502a1
Murray Logan <i.obesulus@gmail.com>
2024-05-30 13:50:07 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 git clone https://github.com/open-AIMS/dh_sediment_monitoring.git ../project1/temp
-cp -r ../project1/temp ../project
-cp -r ../project1/temp/R ../project/R
-cp -r ../project1/temp/md ../project/md
-cp -r ../project1/temp/parameters ../project/parameters
-rm -R ../project1/temp
+#cp -r ../project1/temp ../project
+#cp -r ../project1/temp/R ../project/R
+#cp -r ../project1/temp/md ../project/md
+#cp -r ../project1/temp/parameters ../project/parameters
+#rm -R ../project1/temp
 cd /home/project
 cd R
 Rscript run.R 
